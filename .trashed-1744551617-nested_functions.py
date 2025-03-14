062420a5be4fe12dc1cd44880561e0bf41948857
AldwinOda01 <>
2025-03-14 21:41:22 +0800

    Last Sync: 2025-03-14 13:41:22 (Mobile)

--- .trashed-1744551617-nested_functions.py
+++ .trashed-1744551617-nested_functions.py
@@ -3,15 +3,18 @@ def square(num):
     return num ** 2
 
 def sum_of_squares(numbers):
-   
+
     total = 0
     for n in numbers:
-        total += square(n) 
-        
+        total += square(n)
     return total
+
+def square(num):
+    
+    return num ** 2
 
 numbers_list = [2, 3, 4]
 
 result = sum_of_squares(numbers_list)
 
-print(f"The sum of squares is: {result}")
+print(f"The area of a circle with radius {numbers_list} is: {result}")
